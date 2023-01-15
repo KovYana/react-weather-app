5a2907c285b3add84465fee6f958a94ce2ab4e07
KovYana <rybak.yana@gmail.com>
2023-01-15 20:32:46 +0100

    Import axios

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+    function handleResponse(response){
+        console.log(response.data);
+    }
+
+    let apiKey="5taea11c8398fd054f57oa8d334b853e";
+    let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="WeatherForecast">
             <div className="row">
